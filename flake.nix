c9d1789d87697a959834ec85c38c1e01faa1d135
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-02-11 13:31:02 -0300

    fixing flake.nix

--- flake.nix
+++ flake.nix
@@ -9,38 +9,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: 
-let 
-  system = "x86_64.linux";
-  homeManager = 
-    home-manager.nixosModules.home-manager {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.lauti = import ./home.nix;
-        backupFileExtension = "backup";
-      };
-    };
-in
-  nixosConfigurations = {
-    b360m = {
-      nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./hosts/b360m/configuration.nix
-          homeManager 
-        ];
-      };
-    };
-
-    thinkpad = {
-      nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./hosts/thinkpad/configuration.nix
-          homeManager
-        ];
-      };
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let 
+      mySystem = "x86_64.linux";
+      homeManager = 
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.lauti = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        };
+    in {
+      nixosConfigurations = {
+        b360m = nixpkgs.lib.nixosSystem {
+          system = mySystem;
+          modules = [
+            ./hosts/b360m/configuration.nix
+            homeManager 
+          ];
+        };
+    
+        thinkpad = nixpkgs.lib.nixosSystem {
+          system = mySystem;
+          modules = [
+            ./hosts/thinkpad/configuration.nix
+            homeManager
+          ];
+        };
     };
   };
 }
