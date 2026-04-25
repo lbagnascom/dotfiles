80cb5bc8002d80da234bdb13bf26fb4a759648c5
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-04-25 11:35:48 -0300

    Add devenv setup to enable caching

--- common/configuration.nix
+++ common/configuration.nix
@@ -91,6 +91,12 @@
     };
   };
 
+  # Devenv cache setup
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
+
   # Bluetooth
   hardware.bluetooth = {
     enable = true;
