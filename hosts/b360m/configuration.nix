3b216d19664d595cea33af940c17dfc8a81f3867
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-06-02 15:12:19 -0300

    enable printer for b360m

--- hosts/b360m/configuration.nix
+++ hosts/b360m/configuration.nix
@@ -2,4 +2,18 @@
 
 {
   networking.hostName = "b360m";
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      gutenprint
+      hplip
+    ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
