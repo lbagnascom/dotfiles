891ba75e39b587c0336bfb3924af432d9de7d778
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-08-19 23:07:58 -0300

    automatic cleanup of nix store

--- common/configuration.nix
+++ common/configuration.nix
@@ -131,6 +131,13 @@
     wantedBy = lib.mkForce [ ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  nix.settings.auto-optimise-store = true;
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
